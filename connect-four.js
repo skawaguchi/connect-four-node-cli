4ad74ffe93ee6595e8b9737e15f4427028c598ec
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2023-03-06 16:10:26 -0500

    chore: basic setup using vitest

---
+++ connect-four.js
@@ -0,0 +1,3 @@
+export const start = () => {
+    return 2
+}
---
+++ connect-four.test.js
@@ -0,0 +1,7 @@
+import { start } from './connect-four'
+
+describe('Connect Four', () => {
+    it('should do stuff', () => {
+        expect(start()).toEqual(2)
+    })
+})
