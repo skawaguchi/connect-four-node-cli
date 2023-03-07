c751ccd4f003e0a92215765225ef39373f51ba92
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2023-03-07 13:22:41 -0500

    feat: start game

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # opslevel-pair-programming-interview-redo
+
+Requirements
+- 6 rows, 7 cols
+- 7 cols are slots for users
+- 2 players - yellow or red
+    - ID players by color
+- randomize color + who goes first
+- hotseat game
+- gravity - pieces always drop to the bottom-most unoccupied slot
+- win conditions: diagonally, horizontally, vertically
+- if all slots are filled with no winner, tie
+- can't put more pieces than the column slots available
+
+Design
+- Board - configurable grid (default 6 x 7)
+- Designate color to players + turn order
+- Number slots 1 - length (7)
+- CLI to take input
+    - prompt the correct player when it's their turn
+- Player drops piece
+    - check for valid move
+    - calculate the position
+    - check for a win
+    - if no win pass control to other player
+    - on an invalid move 
--- connect-four.test.js
+++ connect-four.test.js
@@ -1,7 +1,37 @@
-import { start } from './connect-four'
+import { beforeEach } from 'vitest'
+import {
+    chooseStartingPlayer,
+    getBoard,
+    start
+} from './connect-four'
+
+vi.mock('readline')
 
 describe('Connect Four', () => {
-    it('should do stuff', () => {
-        expect(start()).toEqual(2)
+    const consoleInfoStub = vi.spyOn(console, 'info').mockImplementation(() => {})
+
+    beforeEach(() => {
+        consoleInfoStub.mockClear()
+    })
+    it('should build a board with a default of 7 columns and 6 rows with an empty value', () => {
+        const board = getBoard()
+
+        expect(board.length).toEqual(6)
+
+        for (let row = 0; row < board.length; row++) {
+            for (let col = 0; col < 7; col++) {
+                expect(board[row][col]).toBe(null)
+            }
+        }
+    })
+
+    it('should choose "red" as the starting player', () => {
+        expect(chooseStartingPlayer()).toEqual('RED')
+    })
+
+    it('should start the game with a message', () => {
+        start()
+
+        expect(consoleInfoStub).toHaveBeenCalledWith(expect.any(String))
     })
 })
